fce2812328f261be875edc0bfbee1e289484f0dc
mradsgh <49834835+mradsgh@users.noreply.github.com>
2019-11-23 12:00:08 +0000

    Update Powershell incremental backup script.ps1

--- Powershell incremental backup script.ps1
+++ Powershell incremental backup script.ps1
@@ -10,54 +10,81 @@
 ####################
 # create variables #
 ####################
-$currentMonth = Get-Date -UFormat %m;$CurrentMonth = (Get-Culture).DateTimeFormat.GetMonthName($CurrentMonth)
+$currentMonth = Get-Date -UFormat %m
+$currentMonth = (Get-Culture).DateTimeFormat.GetMonthName($currentMonth)
 $CurrentYear = (Get-Date).ToString('yyyy')
 $CurrentWeek = Get-Date -UFormat %V
 $CurrentDateTime = (Get-Date).ToString('yyyy_MM_dd HH_mm')
-$SRCLocation = "S:\" # Change lines 48/72 drive letter to match SRClocation drive letter
-$DSTLocation = "F:\$CurrentWeek - $CurrentMonth $CurrentDateTime"
-$LastFullBackUp = Get-ChildItem "F:\" | Where-Object {$_.CreationTime -gt (Get-Date).AddDays(-7) -and $_.Name -like "*Full Back Up"} | select -Expand Fullname # Change "AddDays(-2)" to "AddDays(-7)" to retrieve last week full backup
+$SRCLocation = 'S:\' # Change lines 48/72 drive letter to match SRClocation drive letter
+$DSTLocation = "F:\$CurrentWeek - $currentMonth $CurrentDateTime"
+$LastFullBackUp = Get-ChildItem -Path 'F:\' |
+Where-Object -FilterScript {
+  $_.CreationTime -gt (Get-Date).AddDays(-7) -and $_.Name -like '*Full Back Up'
+} |
+Select-Object -ExpandProperty Fullname # Change "AddDays(-2)" to "AddDays(-7)" to retrieve last week full backup
 
 ##################################
 # Compare Source and Destination #
 ##################################
-$CompareSource = Get-ChildItem –Path "$SRCLocation" -Recurse
-$CompareFullBackUp = Get-ChildItem –Path "$LastFullBackUp" -Recurse
-$ModifiedObject = Compare-Object -ReferenceObject $CompareSource -DifferenceObject $CompareFullBackUp | ?{$_.sideIndicator -eq "<="} |select inputobject
+$CompareSource = Get-ChildItem -Path "$SRCLocation" -Recurse
+$CompareFullBackUp = Get-ChildItem -Path "$LastFullBackUp" -Recurse
+$ModifiedObject = Compare-Object -ReferenceObject $CompareSource -DifferenceObject $CompareFullBackUp |
+Where-Object -FilterScript {
+  $_.sideIndicator -eq '<='
+} |
+Select-Object -Property inputobject
 
 #######################################
 # Sort files and folders to be copied #
 #######################################
-$ModifiedObject = $ModifiedObject | ForEach { $_.InputObject.FullName}
-$ModifiedObjectFolder = Get-item $ModifiedObject | ?{$_.PSIsContainer -eq "true"} | select -expand fullname # Get folder fullpath
-$ModifiedObjectFile = Get-ChildItem $ModifiedObject | select -expand fullname # Get file fullpath
+$ModifiedObject = $ModifiedObject | ForEach-Object -Process {
+  $_.InputObject.FullName
+}
+$ModifiedObjectFolder = Get-Item $ModifiedObject |
+Where-Object -FilterScript {
+  $_.PSIsContainer -eq 'true'
+} |
+Select-Object -ExpandProperty fullname # Get folder fullpath
+$ModifiedObjectFile = Get-ChildItem $ModifiedObject | Select-Object -ExpandProperty fullname # Get file fullpath
 
 ##################################
 # Create hourly Directory folder #
 ##################################
 $FolderExists = Test-Path $DSTLocation
 If ($FolderExists -eq $False) 
-{New-Item -ItemType directory -Path $DSTLocation -Force}
+{
+  New-Item -ItemType directory -Path $DSTLocation -Force
+}
 
 ########################################################################################
 # Loop through objects to Copy new/modified files or new DIR since last full backup    #
 ########################################################################################
 foreach ($object in $ModifiedObjectFolder)
 {
-$ModifiedObjectPath = $null # Null variable for use
-$ModifiedObjectPath = Split-Path $object;$ModifiedObjectPath = "$ModifiedObjectPath" -replace "^S:", "" # Remove Drive Letter from directory path
+  $ModifiedObjectPath = $null # Null variable for use
+  $ModifiedObjectPath = Split-Path $object
+  $ModifiedObjectPath = "$ModifiedObjectPath" -replace '^S:', '' # Remove Drive Letter from directory path
 
-###################################
-# Create directory in destination #
-###################################
-$DestFolderExists = Test-Path "$DSTLocation$ModifiedObjectPath"# Check if directory is already created
-If ($DestFolderExists -eq $False) 
-{New-Item -ItemType directory -Path "$DSTLocation$ModifiedObjectPath" -Force}
+  ###################################
+  # Create directory in destination #
+  ###################################
+  $DestFolderExists = Test-Path -Path "$DSTLocation$ModifiedObjectPath"# Check if directory is already created
+  If ($DestFolderExists -eq $False) 
+  {
+    New-Item -ItemType directory -Path "$DSTLocation$ModifiedObjectPath" -Force
+  }
 
-###################################
-# Copy folder ACLs to destination #
-###################################
-Get-Acl -Path $object | where { $_.Access | where { $_.IsInherited -eq $false } } | Set-Acl -Path "$DSTLocation$ModifiedObjectPath"
+  ###################################
+  # Copy folder ACLs to destination #
+  ###################################
+  Get-Acl -Path $object |
+  Where-Object -FilterScript {
+    $_.Access |
+    Where-Object -FilterScript {
+      $_.IsInherited -eq $False 
+    } 
+  } |
+  Set-Acl -Path "$DSTLocation$ModifiedObjectPath"
 }
 
 ####################################
@@ -65,18 +92,26 @@ Get-Acl -Path $object | where { $_.Access | where { $_.IsInherited -eq $false } 
 ####################################
 foreach ($object in $ModifiedObjectFile)
 {
-$ModifiedObjectName=$null # Null variable for use
-$ModifiedObjectPath = $null # Null variable for use
+  $ModifiedObjectName = $null # Null variable for use
+  $ModifiedObjectPath = $null # Null variable for use
 
-$ModifiedObjectName = Split-Path $object -Leaf # Store filename in variable
-$ModifiedObjectPath = Split-Path $object;$ModifiedObjectPath = "$ModifiedObjectPath" -replace "^S:", "" # Remove Drive Letter from directory path
+  $ModifiedObjectName = Split-Path -Path $object -Leaf # Store filename in variable
+  $ModifiedObjectPath = Split-Path $object
+  $ModifiedObjectPath = "$ModifiedObjectPath" -replace '^S:', '' # Remove Drive Letter from directory path
 
-Copy-Item -Path $object "$DSTLocation$ModifiedObjectPath\$ModifiedObjectName"
+  Copy-Item -Path $object -Destination "$DSTLocation$ModifiedObjectPath\$ModifiedObjectName"
 
-#################################
-# Copy file ACLs to destination #
-#################################
-Get-Acl -Path $object | where { $_.Access | where { $_.IsInherited -eq $false } } | Set-Acl -Path "$DSTLocation$ModifiedObjectPath\$ModifiedObjectName"
+  #################################
+  # Copy file ACLs to destination #
+  #################################
+  Get-Acl -Path $object |
+  Where-Object -FilterScript {
+    $_.Access |
+    Where-Object -FilterScript {
+      $_.IsInherited -eq $False 
+    } 
+  } |
+  Set-Acl -Path "$DSTLocation$ModifiedObjectPath\$ModifiedObjectName"
 }
 
 ########################################
