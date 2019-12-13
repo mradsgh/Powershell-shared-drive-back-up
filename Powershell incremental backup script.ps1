fc25c39eb27447a47e8932399223028729e8a00a
mradsgh <49834835+mradsgh@users.noreply.github.com>
2019-12-13 17:21:04 +0000

    Update Powershell incremental backup script.ps1

--- Powershell incremental backup script.ps1
+++ Powershell incremental backup script.ps1
@@ -19,7 +19,7 @@ $SRCLocation = 'S:\' # Change lines 48/72 drive letter to match SRClocation driv
 $DSTLocation = "F:\$CurrentWeek - $currentMonth $CurrentDateTime"
 $LastFullBackUp = Get-ChildItem -Path 'F:\' |
 Where-Object -FilterScript {
-  $_.CreationTime -gt (Get-Date).AddDays(-7) -and $_.Name -like '*Full Back Up'
+    $_.CreationTime -gt (Get-Date).AddDays(-7) -and $_.Name -like '*Full Back Up'
 } |
 Select-Object -ExpandProperty Fullname # Change "AddDays(-2)" to "AddDays(-7)" to retrieve last week full backup
 
@@ -30,7 +30,7 @@ $CompareSource = Get-ChildItem -Path "$SRCLocation" -Recurse
 $CompareFullBackUp = Get-ChildItem -Path "$LastFullBackUp" -Recurse
 $ModifiedObject = Compare-Object -ReferenceObject $CompareSource -DifferenceObject $CompareFullBackUp |
 Where-Object -FilterScript {
-  $_.sideIndicator -eq '<='
+    $_.sideIndicator -eq '<='
 } |
 Select-Object -Property inputobject
 
@@ -38,11 +38,11 @@ Select-Object -Property inputobject
 # Sort files and folders to be copied #
 #######################################
 $ModifiedObject = $ModifiedObject | ForEach-Object -Process {
-  $_.InputObject.FullName
+    $_.InputObject.FullName
 }
 $ModifiedObjectFolder = Get-Item $ModifiedObject |
 Where-Object -FilterScript {
-  $_.PSIsContainer -eq 'true'
+    $_.PSIsContainer -eq 'true'
 } |
 Select-Object -ExpandProperty fullname # Get folder fullpath
 $ModifiedObjectFile = Get-ChildItem $ModifiedObject | Select-Object -ExpandProperty fullname # Get file fullpath
@@ -51,67 +51,63 @@ $ModifiedObjectFile = Get-ChildItem $ModifiedObject | Select-Object -ExpandPrope
 # Create hourly Directory folder #
 ##################################
 $FolderExists = Test-Path $DSTLocation
-If ($FolderExists -eq $False) 
-{
-  New-Item -ItemType directory -Path $DSTLocation -Force
+If ($FolderExists -eq $False) {
+    New-Item -ItemType directory -Path $DSTLocation -Force
 }
 
 ########################################################################################
 # Loop through objects to Copy new/modified files or new DIR since last full backup    #
 ########################################################################################
-foreach ($object in $ModifiedObjectFolder)
-{
-  $ModifiedObjectPath = $null # Null variable for use
-  $ModifiedObjectPath = Split-Path $object
-  $ModifiedObjectPath = "$ModifiedObjectPath" -replace '^S:', '' # Remove Drive Letter from directory path
+foreach ($object in $ModifiedObjectFolder) {
+    $ModifiedObjectPath = $null # Null variable for use
+    $ModifiedObjectPath = Split-Path $object
+    $ModifiedObjectPath = "$ModifiedObjectPath" -replace '^S:', '' # Remove Drive Letter from directory path
 
-  ###################################
-  # Create directory in destination #
-  ###################################
-  $DestFolderExists = Test-Path -Path "$DSTLocation$ModifiedObjectPath"# Check if directory is already created
-  If ($DestFolderExists -eq $False) 
-  {
-    New-Item -ItemType directory -Path "$DSTLocation$ModifiedObjectPath" -Force
-  }
+    ###################################
+    # Create directory in destination #
+    ###################################
+    $DestFolderExists = Test-Path -Path "$DSTLocation$ModifiedObjectPath"# Check if directory is already created
+    If ($DestFolderExists -eq $False) {
+        New-Item -ItemType directory -Path "$DSTLocation$ModifiedObjectPath" -Force
+    }
 
-  ###################################
-  # Copy folder ACLs to destination #
-  ###################################
-  Get-Acl -Path $object |
-  Where-Object -FilterScript {
-    $_.Access |
+    ###################################
+    # Copy folder ACLs to destination #
+    ###################################
+    Get-Acl -Path $object |
     Where-Object -FilterScript {
-      $_.IsInherited -eq $False 
-    } 
-  } |
-  Set-Acl -Path "$DSTLocation$ModifiedObjectPath"
+        $_.Access |
+        Where-Object -FilterScript {
+            $_.IsInherited -eq $False 
+        } 
+    } |
+    Set-Acl -Path "$DSTLocation$ModifiedObjectPath"
 }
 
 ####################################
 # Copy files to backup destination #
 ####################################
-foreach ($object in $ModifiedObjectFile)
-{
-  $ModifiedObjectName = $null # Null variable for use
-  $ModifiedObjectPath = $null # Null variable for use
+foreach ($object in $ModifiedObjectFile) {
+    $ModifiedObjectName = $null # Null variable for use
+    $ModifiedObjectPath = $null # Null variable for use
 
-  $ModifiedObjectName = Split-Path -Path $object -Leaf # Store filename in variable
-  $ModifiedObjectPath = Split-Path $object
-  $ModifiedObjectPath = "$ModifiedObjectPath" -replace '^S:', '' # Remove Drive Letter from directory path
+    $ModifiedObjectName = Split-Path -Path $object -Leaf # Store filename in variable
+    $ModifiedObjectPath = Split-Path $object
+    $ModifiedObjectPath = "$ModifiedObjectPath" -replace '^S:', '' # Remove Drive Letter from directory path
 
-  Copy-Item -Path $object -Destination "$DSTLocation$ModifiedObjectPath\$ModifiedObjectName"
+    Copy-Item -Path $object -Destination "$DSTLocation$ModifiedObjectPath\$ModifiedObjectName"
 
-  #################################
-  # Copy file ACLs to destination #
-  #################################
-  Get-Acl -Path $object |
-  Where-Object -FilterScript {
-    $_.Access |
+    #################################
+    # Copy file ACLs to destination #
+    #################################
+    Get-Acl -Path $object |
     Where-Object -FilterScript {
-      $_.IsInherited -eq $False 
-    } 
-  } |
-  Set-Acl -Path "$DSTLocation$ModifiedObjectPath\$ModifiedObjectName"
+        $_.Access |
+        Where-Object -FilterScript {
+            $_.IsInherited -eq $False 
+        } 
+    } |
+    Set-Acl -Path "$DSTLocation$ModifiedObjectPath\$ModifiedObjectName"
 }
 
 ########################################
